68401ed9d9714c4d4824d6c2b2dd0ba8f43c23b1
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-12-12 10:05:37 +0100

    audit

--- importLignesCommandes.py
+++ importLignesCommandes.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+
 import sys
 from simple_salesforce import (
     Salesforce,
@@ -12,5 +15,25 @@ from simple_salesforce import (
     SalesforceMalformedRequest
 )
 
+import csv
+
+
+def audit():
+    sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
+    
+    allProduits = []
+    allComptes =[]
+    allSorifa =[]
+    with open('./archive_ldc.csv','r') as f: # Internet2017.csv venteshisto.csv        
+        reader = csv.DictReader(f, delimiter=';')
+        for l in reader:
+            if l['n°client facturé'] not in allSorifa:
+                allSorifa.append(l['n°client facturé'])
+            if l['N°client livré'] not in allSorifa:
+                allSorifa.append(l['N°client livré'])
+            if l['référence'] not in allProduits:
+                allProduits.append(l['référence'])
+    print('Comptes uniques',len(allSorifa))
+    print('Produits uniques',len(allProduits))
 if __name__=='__main__':
-  pass
+  audit()
