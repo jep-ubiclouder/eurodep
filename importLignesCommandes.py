1d53800f0611a914ad51cbcff179a37b733656f5
jep-ubiclouder <jep-ubiclouder@users.noreply.github.com>
2017-12-12 09:03:43 +0100

    Create importLignesCommandes.py
    pour le gros fichier d'archive lignes de commandes

---
+++ importLignesCommandes.py
@@ -0,0 +1,16 @@
+import sys
+from simple_salesforce import (
+    Salesforce,
+    SalesforceAPI,
+    SFType,
+    SalesforceError,
+    SalesforceMoreThanOneRecord,
+    SalesforceExpiredSession,
+    SalesforceRefusedRequest,
+    SalesforceResourceNotFound,
+    SalesforceGeneralError,
+    SalesforceMalformedRequest
+)
+
+if __name__=='__main__':
+  pass
