f2a9e5bdadf6a039ad635aa27cec819df99db840
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-02-15 15:36:45 +0100

    first commit: check arcelor

--- byAccount.py
+++ byAccount.py
@@ -14,3 +14,15 @@ from simple_salesforce import (
 
 import csv
 import json
+
+def checkAccount(strAccId):
+    forAccount = []
+    with open('./archive_ldc.csv','r') as f: # Internet2017.csv venteshisto.csv        
+        reader = csv.DictReader(f, delimiter=';')
+        for l in reader:
+            if l['n°client facturé']  == strAccId:
+                forAccount.append(l)
+    print(forAccount)
+
+if __name__ == '__main__':
+    checkAccount('010643')
