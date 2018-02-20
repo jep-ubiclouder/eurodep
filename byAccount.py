4c37e1d22b4eaedbdc8db062846b962ad0ec7b2c
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-02-20 09:36:34 +0100

    test2

--- byAccount.py
+++ byAccount.py
@@ -23,10 +23,22 @@ def checkAccount(strAccId):
         reader = csv.DictReader(f, delimiter=';')
         for l in reader:
             if l['n°client facturé']  == strAccId:
-                forAccount.append(l)
-    
+                
+                ## forAccount.append(l)
+                if float(l['remis ligne']) >0 : remLign=float(l['remis ligne'])
+                else:  remLign=0.00
+                if float(l['remise pied']) >0 : remPied=float(l['remise pied'])
+                else:  remPied=0.00
+                record={'remise' : (1-remLign)*(1-remPied),'prix untaire brut':float(l['prix untaire brut']),'dateCommande':l['date document']}
+                forAccount.append(record)
+                
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(forAccount)
+    
+    
+    
+    
+    
     print(len(forAccount))
 if __name__ == '__main__':
     checkAccount('007798')
