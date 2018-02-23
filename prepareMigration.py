81ed08d86f2f996618be20286544aec0e91e6e9e
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-02-23 09:20:15 +0100

    let's rock

--- prepareMigration.py
+++ prepareMigration.py
@@ -1,1 +1,39 @@
+from simple_salesforce import (
+    Salesforce,
+    SalesforceAPI,
+    SFType,
+    SalesforceError,
+    SalesforceMoreThanOneRecord,
+    SalesforceExpiredSession,
+    SalesforceRefusedRequest,
+    SalesforceResourceNotFound,
+    SalesforceGeneralError,
+    SalesforceMalformedRequest
+)
+
 import sys
+from _datetime import timedelta
+from datetime import date
+from code import banner
+
+
+def goDelete(sf):
+    
+    for annee in range(2002,2011):
+        for mois in range(1,13):
+            qry  = 'SELECT Date_de_commande__c ,id,Year_Month__c FROM Commande__c where Year_Month__c =' +'%s'%((annee*100)+mois)
+            records = sf.query_all(qry)['records']
+            tobedel = []
+            for r in records :
+                tobedel.append({'Id':r['Id']})
+                print((annee*100)+mois,'lignes:',len(tobedel))
+                try:
+                    sf.bulk.Commande__c.delete(tobedel)
+                except Exception as e:
+                    print(len(tobedel))
+
+    
+
+if __name__ == '__main__':
+    sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
+    goDelete(sf)
