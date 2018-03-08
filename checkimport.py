34553573524c3d7b262696cb5bc0fd1a1524650c
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-03-08 11:19:26 +0100

    import salesforce

--- checkimport.py
+++ checkimport.py
@@ -1,6 +1,18 @@
 import sys
 import csv
 import json
+from simple_salesforce import (
+    Salesforce,
+    SalesforceAPI,
+    SFType,
+    SalesforceError,
+    SalesforceMoreThanOneRecord,
+    SalesforceExpiredSession,
+    SalesforceRefusedRequest,
+    SalesforceResourceNotFound,
+    SalesforceGeneralError,
+    SalesforceMalformedRequest
+)
 
 
 if __name__ == '__main__':
