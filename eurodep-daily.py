453c1c16e4364a88fbf057c42ef73d9f2a9db9f1
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-05-18 16:57:30 +0200

    config.ini

--- eurodep-daily.py
+++ eurodep-daily.py
@@ -418,7 +418,11 @@ def PruneAndGraft():
             sf.bulk.Contact.update(updateContacts)
 def connectLignes():
     """ Essai de reconnecter les lignes aux comptes"""
-    sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
+    
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    sf = Salesforce(username=config['DEFAULT']['username'], password=config['DEFAULT']['password'], security_token=config['DEFAULT']['security_token'])
     
     qry = 'select id, Code_Client_EURODEP__c,Compte__c from commande__c where Code_Client_EURODEP__c !=null  and Compte__c = null' 
     allEurodep = []
