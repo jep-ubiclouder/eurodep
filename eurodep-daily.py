32c500a04f3437bf3c6578f1291185f99d3211a4
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-05-18 16:42:54 +0200

    jmastio@homme-de-fer.com pour les Comptes inconnus

--- eurodep-daily.py
+++ eurodep-daily.py
@@ -133,7 +133,7 @@ def envoieEmailCI(clientsInconnus):
     msg = MIMEText(texteHTML, 'html')
     msg['Subject'] = 'Lignes Integrées'
     msg['From'] = 'salesforce@homme-de-fer.com' 
-    msg['To'] = 'lbronner@homme-de-fer.com, jep@ubiclouder.com,     jmastio@sfsorifa.com,    mlabarthe@homme-de-fer.com' ## , dKannengieser@asyspro.fr, adevisme@homme-de-fer.com, dk@asyspro.com'
+    msg['To'] = 'lbronner@homme-de-fer.com, jep@ubiclouder.com,     jmastio@homme-de-fer.com,    mlabarthe@homme-de-fer.com' ## , dKannengieser@asyspro.fr, adevisme@homme-de-fer.com, dk@asyspro.com'
     # Send the message via our own SMTP server.
     ## s = smtplib.SMTP(host='smtp.dsl.ovh.net',port=25)
     s =  smtplib.SMTP(host='smtp.homme-de-fer.com',port=25)
