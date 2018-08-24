568e59f77f8cdca7c636341713ab89873215a310
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-08-24 16:32:14 +0200

    sdf

--- ExtractPBentries.py
+++ ExtractPBentries.py
@@ -37,7 +37,7 @@ if __name__=='__main__':
         
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
     qry =  'select Id , Name,Pricebook2Id,Product2Id,unitPrice,isActive from PricebookEntry where isActive = true'
-    records =  qf.query_all(qry)['records']
+    records =  sf.query_all(qry)['records']
     for r  in records:
         print(r)
         line += 1
