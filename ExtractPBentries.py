c235b8be996dce62a4ace90b20b05fdd81939d39
jep-ubiclouder <jep-ubiclouder@users.noreply.github.com>
2018-11-16 09:53:02 +0100

    Update : send file via mail.

--- ExtractPBentries.py
+++ ExtractPBentries.py
@@ -20,9 +20,46 @@ import codecs
 import os.path
 import csv
 import xlsxwriter
-if __name__=='__main__':
+
+def envoieMail(fileName):
+    ''' Envoie une liste des lignes traitées'''
+    import smtplib
+    # [r['CODCLI'],r['NOM'],r['ADRESSE'],r['CP'],r['VILLE']]
+    texteHTML = """
+    Bonjour,<br/>
+    Voici le fichier pour vous permettre d'établir les tarifs.<br/>
+    Ce fichier est envoyé tout les mois. Il contient les prix des tarifs qui sont en ce moment dans Salesforce.<br/>
+    """
+    from email.mime.text import MIMEText 
+    msg = MIMEMultipart()## MIMEText(texteHTML, 'html')
+    msg.attach(MIMEText(texteHTML))
     
-    workbook =  xlsxwriter.Workbook('ElementsDeTarif.xlsx')
+    
+    msg['Subject'] = 'Extraction fichier travail pour tarif'
+    msg['From'] = 'salesforce@homme-de-fer.com'
+    # , dKannengieser@asyspro.fr, adevisme@homme-de-fer.com, dk@asyspro.com'
+    msg['To'] = 'lbronner@homme-de-fer.com, jep@ubiclouder.com'
+    
+    part = MIMEBase('application', "octet-stream")
+    with open(fileName, 'rb') as file:
+        part.set_payload(file.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition','attachment; filename="{}"'.format(op.basename(path)))
+        msg.attach(part)
+    # Send the message via our own SMTP server.
+    ## s = smtplib.SMTP(host='smtp.dsl.ovh.net',port=25)
+    s = smtplib.SMTP(host='smtp.homme-de-fer.com', port=25)
+    s.login('salesforce@homme-de-fer.com', 'S@lf0rc3!')
+    s.send_message(msg)
+    s.quit()
+    print('Email lignes envoyé')
+
+if __name__=='__main__':
+    mois=['','Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Août','Spetembre','Octobre','Novembre','Decembre']
+    now = datetime.datetime.now()
+    fileName= 'ElementsDeTarif-%s-%s.xlsx' %(now.year,mois[now.month])
+    
+    workbook =  xlsxwriter.Workbook(fileName)
     
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
     
@@ -81,5 +118,6 @@ if __name__=='__main__':
             sheet3.write(line,col,r[label])
             col += 1
     workbook.close()
+    envoieMail(fileName)
 
         
